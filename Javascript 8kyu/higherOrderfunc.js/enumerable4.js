1372f490b366cbc13af9b11957a1370de7ae8cb8
Yesenia L <ylopezcr123@gmail.com>
2021-12-12 21:44:34 -0500

    enumerable4

---
+++ Javascript 8kyu/higherOrderfunc.js/enumerable4.js
@@ -0,0 +1,12 @@
+//const none = (array, fun) => !array.some(fun)
+
+function none(arr, fun) {
+    return !arr.some(fun)
+} 
+console.log(none([1,2,3,4,5],function(item){ return item > 5 })) //, true)
+console.log(none([1,2,3,4,5],function(item){ return item > 4 })) //, false)
+
+/**
+ * Yesis pseudo code
+ * check if some of the elements in arr are true to function
+ */
