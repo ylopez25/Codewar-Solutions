36db26afb660fb90113bd0d737302da79a8b1469
Yesenia L <ylopezcr123@gmail.com>
2022-07-27 19:39:43 -0400

    findDupes

--- dsa/leetcode/findDupes.js
+++ dsa/leetcode/findDupes.js
@@ -1,24 +1,25 @@
-FIX this!!
-
 function duplicates(arr) {
-    //TODO: return the array of duplicates from arr
-    let obj = {};
-    let dupes =[];
-   for(const el of arr){
-if (obj[el] = el && obj[el] === 'number' ) {
-    obj[el] += 1;
-    console.log(obj)
-}
 
-}
-    console.log(obj)
+let uniqueEl = new Set(arr);
+let filteredArr = arr.filter(el => {
+    if(uniqueEl.has(el)) {
+        uniqueEl.delete(el)
+        console.log(uniqueEl)
+    }else {
+        console.log(el)
+        return el;
 
-    for(let key in obj) {
-        if(obj[key] > 1) {
-            dupes.push(Number(key));
-            console.log(typeof dupes)
-        }
     }
-    return dupes
+})
+
+return [...new Set(filteredArr)]
+
 }
   console.log(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, '5']))
+
+  //psuedo code
+  // iterate through arr
+  //create a hashmap that will have the el of arr as keys and repition as values;
+  //i want to return any keys that have el more than 2
+  //handle strings 
+  //
