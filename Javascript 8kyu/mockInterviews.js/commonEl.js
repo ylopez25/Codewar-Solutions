fd5944213a361025af906bf46e6586ba0666c518
Yesenia L <ylopezcr123@gmail.com>
2023-12-27 21:21:18 -0500

    console pt3

--- Javascript 8kyu/mockInterviews.js/commonEl.js
+++ Javascript 8kyu/mockInterviews.js/commonEl.js
@@ -22,4 +22,3 @@ function commonElements(arr1,arr2) {
     
     console.log(commonElements([2,3,4],[3,4,5]));
     console.log(commonElements([1,2,3,3], [3,4,6]) === [3]);
-    console.log(commonElements([1,2,3],[4,4]) === null)    
